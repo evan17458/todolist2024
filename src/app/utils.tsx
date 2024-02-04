28eb1ab5f2a28f804631e66d093015b7218aef53
evan17458 <a0952987429@yahoo.com.tw>
2024-02-04 22:00:27 +0800

    修正

--- src/app/utils.tsx
+++ src/app/utils.tsx
@@ -3,7 +3,6 @@ import moment from "moment";
 export const formatDate = (): string => {
   return moment().format("YYYY-MM-DDTHH:mm:ss[Z]");
 };
-
 export const handleError = (error: any): void => {
   console.error(error);
 };
